f9e7f6759b9d3a5bef48b5b84b81835ea99ba2f4
EthanAdler <EthanAdler@users.noreply.github.com>
2017-03-21 08:29:15 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -1,1 +1,20 @@
 
+#   This reads and prints the whole document.
+#           It does not prt a blank line after each line of the file
+
+name_of_mydocument = 'tuesdayafternoon.txt'
+file_input = open(name_of_mydocument, 'r')     #open file for reading
+
+line_counter = 0
+
+line = file_input.readline()
+line = file_input.readline()
+
+
+
+while line != '':                      # while not end of file
+    line_counter += 1
+    print(line_counter , line, end = '')              # don't print another new line
+    line = file_input.readline()
+
+file_input.close()
