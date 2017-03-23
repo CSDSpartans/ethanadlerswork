63c0823d914e62c425473f802eb0a9347ae51a42
EthanAdler <EthanAdler@users.noreply.github.com>
2017-03-23 08:18:02 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -1,50 +1,40 @@
-
-#           It does not prt a blank line after each line of the file
-
 name_of_mydocument = 'tuesdayafternoon.txt'
-file_input = open(name_of_mydocument, 'r')     #open file for reading
+file_input = open(name_of_mydocument, 'r')     
+
+
 
 line = file_input.readline()
-print(line, end = '')
+print("      ", line, end = '')
+line = file_input.readline()
+print("      ", line, end = '')
 line = file_input.readline()
 
-print(line, end = '') 
-line = file_input.readline()
-print(line)
 
-line = file_input.readline()
 line_counter = 0
-stanza_counter = 1
+stanza_counter = 0
+total_lines_in_file = 2
 
-while line != '':                      # while not end of file
-
+while line != '':                      
+    total_lines_in_file += 1
     if line == '\n':
       stanza_counter += 1
       print ()
     else:
       line_counter += 1
-      print(line_counter, ") ", line, end = '')              # don't print another new line
+      if line_counter < 10:
+        print(line_counter, ")   ", line, end = '')
+      else:
+        print(line_counter, ")  ", line, end = '')
+    
     line = file_input.readline()
     
+  
+
+
+
 print ()
 print ()
-print ("The number of stanzas is ",  stanza_counter)
+print ("The total number of stanzas in this poem are: ",  stanza_counter, '.')
+print ("The total number of lines are: ", total_lines_in_file, '.')
 
 file_input.close()
-                         
-                          #
-                          #  Code Added to Write Poem
-file_input = open(name_of_mydocument, 'r')
-
-name_of_outputdocument = 'myoutputdocument.txt'
-file_output = open(name_of_outputdocument, 'w')
-
-whole_poem = file_input.read()
-file_input.close()
-
-file_output.write("This is a Copy of the Whole Poem\n\n")
-file_output.write(whole_poem)
-file_output.write("\n\n")
-file_output.write("The number of stanzas is: ")
-file_output.write(str(stanza_counter))
-file_output.close()
